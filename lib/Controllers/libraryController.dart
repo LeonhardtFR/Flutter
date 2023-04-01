3cc2346f058f7da781eee85f2927dc4103e0654b
BEZIAT Maxime <55063874+LeonhardtFR@users.noreply.github.com>
2023-04-01 23:03:42 +0200

    AlbumTab OK

---
+++ lib/Controllers/libraryController.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+
+class LibraryController extends GetxController {
+  var tabIndex = 0.obs;
+  var albumTab = false.obs;
+  var albumListIndex = 0.obs;
+}
--- lib/Screens/Library/library.dart
+++ lib/Screens/Library/library.dart
@@ -6,6 +6,7 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:osbrosound/Controllers/libraryController.dart';
 import 'package:osbrosound/Controllers/playerController.dart';
 import 'package:osbrosound/Helpers/audio_query.dart';
 import 'package:osbrosound/Screens/Player/Player.dart';
@@ -15,9 +16,14 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:logging/logging.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
+
 import '../Player/MiniPlayer.dart';
 
 late AudioHandler _audioHandler;
+
+// Appbar controller
+// TabController? tabController;
+// var libraryController = Get.put(LibraryController());
 
 // Future<void> audioHandler() async {
 //   _audioHandler = await AudioService.init(
@@ -35,7 +41,9 @@ late AudioHandler _audioHandler;
 // }
 
 class LibraryPage extends StatefulWidget {
-  const LibraryPage({Key? key}) : super(key: key);
+  final String? title;
+  final List<SongModel>? takeAlbum;
+  const LibraryPage({Key? key, this.title, this.takeAlbum}) : super(key: key);
 
   @override
   State<LibraryPage> createState() => _LibraryPageState();
@@ -48,8 +56,8 @@ class _LibraryPageState extends State<LibraryPage>
 
   String? tempPath = '/storage/emulated/0/Music';
 
-  // Appbar controller
-  TabController? _tabController;
+  // // Appbar controller
+  TabController? tabController;
 
   // Variable qui va détecter si il y a de la musique dans la librairie
   OfflineAudioQuery offlineAudioQuery = OfflineAudioQuery();
@@ -60,11 +68,11 @@ class _LibraryPageState extends State<LibraryPage>
   List<PlaylistModel> playlistDetails = [];
 
   String _albums = "";
-  final Map<String, List<SongModel>> _albumsList = {};
+  Map<String, List<SongModel>> listAlbums = {};
   final Map<String, List<SongModel>> _artistsList = {};
   final Map<String, List<SongModel>> _genresList = {};
 
-  final List<String> _sortedAlbumList = [];
+  List<String> sortedlistAlbums = [];
   final List<String> _sortedArtistList = [];
   final List<String> _sortedGenreList = [];
 
@@ -72,16 +80,28 @@ class _LibraryPageState extends State<LibraryPage>
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      var libraryController = Get.put(LibraryController());
     // audioHandler();
-    _tabController = TabController(length: 4, vsync: this);
+      print("ALED");
+      print(libraryController.tabIndex.value);
+      tabController = TabController(length: 4, vsync: this, initialIndex: libraryController.tabIndex.value);
     getMusic();
+
+      // if(libraryController.albumTab.value) {
+      //   tabController?.index = 1;
+      // }
+
     });
+  }
+
+  void stateChanged() {
+    setState(() {});
   }
 
   @override
   void dispose() {
     super.dispose();
-    _tabController!.dispose();
+    tabController!.dispose();
   }
 
   Future<void> getMusic() async {
@@ -91,9 +111,15 @@ class _LibraryPageState extends State<LibraryPage>
       await Permission.manageExternalStorage.request();
       await offlineAudioQuery.requestPermission();
 
-      listSongs = List<SongModel>.from(await offlineAudioQuery.getSongs(
-          sortType: SongSortType.DISPLAY_NAME,
-          orderType: OrderType.ASC_OR_SMALLER));
+      // if(widget.takeAlbum != null) {
+      //   Logger.root.info("User take album from album page");
+      //   listSongs = widget.takeAlbum!;
+      // } else {
+        Logger.root.info("List all songs from library");
+        listSongs = List<SongModel>.from(await offlineAudioQuery.getSongs(
+            sortType: SongSortType.DISPLAY_NAME,
+            orderType: OrderType.ASC_OR_SMALLER));
+      // }
     } catch (e) {
       Logger.root.severe("Error while requesting permission: $e");
     }
@@ -105,11 +131,31 @@ class _LibraryPageState extends State<LibraryPage>
         musicExist = true;
       });
     }
+    getAlbums(listSongs);
+  }
+
+
+  Future<void> getAlbums(listSongs) async {
+    for(int i = 0; i < listSongs.length; i++) {
+      try {
+        if(listAlbums.containsKey(listSongs[i].album ?? "Unknown")) {
+          listAlbums[listSongs[i].album ?? "Unknown"]!.add(listSongs[i]);
+        } else {
+          listAlbums[listSongs[i].album ?? "Unknown"] = [listSongs[i]];
+          sortedlistAlbums.add(listSongs[i].album ?? "Unknown");
+        }
+      } catch (e) {
+        print("Error while adding song to album: $e");
+      }
+    }
+
+    // Maintenant, vous pouvez utiliser la variable `albums` qui contient tous les albums avec les chansons correspondantes
   }
 
   // WIDGET BUILD METHOD \\
   @override
   Widget build(BuildContext context) {
+    var libraryController = Get.put(LibraryController());
     var controller = Get.put(PlayerController());
     return Scaffold(
       body: Column(
@@ -117,6 +163,7 @@ class _LibraryPageState extends State<LibraryPage>
           Expanded(
               child: DefaultTabController(
             length: 4,
+            initialIndex: libraryController.tabIndex.value,
             child: Scaffold(
               backgroundColor: Colors.black,
               appBar: AppBar(
@@ -127,12 +174,13 @@ class _LibraryPageState extends State<LibraryPage>
                   labelColor: Colors.white,
                   indicatorColor: Colors.blueAccent,
                   indicatorSize: TabBarIndicatorSize.label,
-                  controller: _tabController,
-                  onTap: (int index) {
-                    setState(() {
-                      _tabController!.index = index;
-                    });
-                  },
+                  controller: tabController,
+    onTap: (int index) {
+      setState(() {
+        tabController!.index = index;
+      });
+    },
+
                   tabs: const [
                     Tab(text: 'Songs'),
                     Tab(text: 'Albums'),
@@ -157,13 +205,13 @@ class _LibraryPageState extends State<LibraryPage>
                       // child: Text('No music found in the storage', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),)
                     )
                   : TabBarView(
-                      controller: _tabController,
+                      controller: tabController,
                       children: [
-                        MusicTab(listSongs: listSongs, tempPath: tempPath!),
+                        MusicTab(listSongs: listSongs, tempPath: tempPath!,),
                         AlbumsTab(
                             tempPath: tempPath!,
-                            albums: _albumsList,
-                            albumsList: _sortedAlbumList),
+                            albums: listAlbums,
+                            albumsList: sortedlistAlbums),
                         const Center(
                             child: Text('Artists',
                                 style: TextStyle(color: Colors.white))),
@@ -210,6 +258,7 @@ class _LibraryPageState extends State<LibraryPage>
 class MusicTab extends StatefulWidget {
   final String tempPath;
   List<SongModel> listSongs = [];
+
 
   MusicTab({Key? key, required this.listSongs, required this.tempPath})
       : super(key: key);
@@ -298,44 +347,70 @@ class _AlbumsTabState extends State<AlbumsTab>
   @override
   bool get wantKeepAlive => true;
 
+  void initState() {
+    super.initState();
+    Get.put(LibraryController());
+  }
+
   @override
   Widget build(BuildContext context) {
+    var libraryController = Get.put(LibraryController());
     super.build(context);
-    return ListView.builder(
-      physics: const BouncingScrollPhysics(),
-      padding: const EdgeInsets.only(top: 20, bottom: 10),
-      shrinkWrap: true,
-      itemExtent: 70.0,
-      itemCount: widget.albumsList.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: OfflineAudioQuery.offlineArtworkWidget(
-            id: widget.albums[widget.albumsList[index]]![0].id,
-            type: ArtworkType.AUDIO,
-            tempPath: widget.tempPath,
-            fileName:
-                widget.albums[widget.albumsList[index]]![0].displayNameWOExt,
-          ),
-          title: Text(
-            widget.albumsList[index],
-            overflow: TextOverflow.ellipsis,
-          ),
-          subtitle: Text(
-            '${widget.albums[widget.albumsList[index]]!.length} songs',
-          ),
-          onTap: () {
-            // Navigator.push(
-            //   context,
-            //   MaterialPageRoute(
-            //     builder: (context) => DownloadedSongs(
-            //       title: widget.albumsList[index],
-            //       cachedSongs: widget.albums[widget.albumsList[index]],
-            //     ),
-            //   ),
-            // );
-          },
-        );
-      },
-    );
+
+    // Erreur si aucun album trouvé
+    if(widget.albumsList.isEmpty) {
+      return const Center(
+        child: Text('No albums found in the storage',
+            style: TextStyle(color: Colors.white)),
+      );
+    }
+
+
+    if(libraryController.albumTab.value == false) {
+      return ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.only(top: 20, bottom: 10),
+        shrinkWrap: true,
+        itemExtent: 70.0,
+        itemCount: widget.albumsList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: OfflineAudioQuery.offlineArtworkWidget(
+              id: widget.albums[widget.albumsList[index]]![0].id,
+              type: ArtworkType.AUDIO,
+              tempPath: widget.tempPath,
+              fileName: widget.albums[widget.albumsList[index]]![0]
+                  .displayNameWOExt,
+            ),
+            title: Text(
+              widget.albumsList[index],
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(color: Colors.white),
+            ),
+            subtitle: Text(
+              '${widget.albums[widget.albumsList[index]]!.length} songs',
+              style: const TextStyle(color: Colors.white),
+            ),
+            onTap: () {
+              libraryController.albumTab(true);
+              libraryController.albumListIndex.value = index;
+              libraryController.tabIndex.value = 1;
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      LibraryPage(
+                        title: widget.albumsList[index],
+                        takeAlbum: widget.albums[widget.albumsList[index]]!,
+                      ),
+                ),
+              );
+            },
+          );
+        },
+      );
+    } else {
+      return MusicTab(listSongs: widget.albums[widget.albumsList[libraryController.albumListIndex.value]]!, tempPath: widget.tempPath);
+    }
   }
 }
