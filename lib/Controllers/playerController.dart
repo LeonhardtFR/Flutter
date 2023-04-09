9df6576fb15fc124eae1cfef41ea4f5526a78957
BEZIAT Maxime <55063874+LeonhardtFR@users.noreply.github.com>
2023-04-09 23:17:37 +0200

    Amelioration affichage duree musique (ne compte les heures que si la musiques depasse les 59mn59s)

--- lib/Controllers/playerController.dart
+++ lib/Controllers/playerController.dart
@@ -1,5 +1,6 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -30,14 +31,15 @@ class PlayerController extends GetxController {
 
   updatePosition() {
     audioPlayer.durationStream.listen((d) {
-      duration(d.toString().split('.').first);
-      maxDuration.value = d!.inSeconds.toDouble();
+      duration(formatDuration(d));
+      maxDuration.value = d?.inSeconds.toDouble() ?? 0;
     });
     audioPlayer.positionStream.listen((p) {
-      position(p.toString().split('.').first);
-      value.value = p!.inSeconds.toDouble();
+      position(formatDuration(p));
+      value.value = p.inSeconds.toDouble() ?? 0;
     });
   }
+
 
   changeDurationToSeconds(seconds) {
     var duration = Duration(seconds: seconds);
@@ -47,7 +49,6 @@ class PlayerController extends GetxController {
   playMusic(songs, index) {
     playIndex.value = index;
     Uri uriSong = Uri.parse(songs.uri.toString());
-    print('Test : ' + songs.id.toString());
     try {
       audioPlayer.setAudioSource(AudioSource.uri(uriSong,
           tag: MediaItem(
@@ -61,33 +62,6 @@ class PlayerController extends GetxController {
     } catch (e) {
       print("Error");
     }
-  }
-
-  Future<void> switchToOfflinePlayer(List<String> paths) async {
-    final items = paths
-        .map((path) => MediaItem(
-        id: path,
-        album: "Offline Album",
-        title: "Offline Song",
-        artUri: Uri.parse("https://example.com/cover_art.jpg")))
-        .toList();
-
-    await audioPlayer.setAudioSource(
-      ConcatenatingAudioSource(children: items.map((item) => AudioSource.uri(Uri.file(item.id), tag: item)).toList()),
-    );
-  }
-
-  Future<void> switchToRadioPlayer(String url) async {
-    final mediaItem = MediaItem(
-      id: url,
-      album: "Bankable Radio",
-      title: "Live Stream",
-      artUri: Uri.parse("https://example.com/cover_art.jpg"),
-    );
-
-    await audioPlayer.setAudioSource(
-      AudioSource.uri(Uri.parse(url), tag: mediaItem),
-    );
   }
 
   Future<void> stopPlayer() async {
@@ -104,91 +78,18 @@ class PlayerController extends GetxController {
   }
 }
 
+// verifie si la duree est null (si oui retourn 00:00) sinom
+// on verifie si le nbre d'heures est >= 1 (si oui on affiche les heures)
+// sinom on affiche que les minutes et secondes
+String formatDuration(Duration? d) {
+  if (d == null) return "00:00";
+  int hours = d.inHours;
+  int minutes = d.inMinutes.remainder(60);
+  int seconds = d.inSeconds.remainder(60);
 
+  String formattedHours = hours > 0 ? "${hours.toString().padLeft(2, '0')}:": "";
+  String formattedMinutes = minutes.toString().padLeft(2, '0');
+  String formattedSeconds = seconds.toString().padLeft(2, '0');
 
-
-
-
-
-
-
-
-// import 'package:audio_service/audio_service.dart';
-// import 'package:get/get.dart';
-// import 'package:just_audio/just_audio.dart';
-// import 'package:on_audio_query/on_audio_query.dart';
-// import 'package:osbrosound/Helpers/audio_query.dart';
-// import 'package:permission_handler/permission_handler.dart';
-//
-// import '../AudioManager.dart';
-//
-
-
-// class PlayerController extends GetxController {
-//   final audioQuery = OnAudioQuery();
-//   final audioPlayer = AudioPlayer();
-//
-//   var playIndex = 0.obs;
-//   var isPlaying = false.obs;
-//
-//   var duration = ''.obs;
-//   var position = ''.obs;
-//
-//   var maxDuration = 0.0.obs;
-//   var value = 0.0.obs;
-//
-//   var player = false.obs;
-//   var miniPlayer = false.obs;
-//
-//   var showMiniPlayer = false.obs;
-//
-//   @override
-//   void onInit() {
-//     super.onInit();
-//     checkPermission();
-//   }
-//
-//   updatePosition() {
-//     audioPlayer.durationStream.listen((d) {
-//       duration(d.toString().split('.').first);
-//       maxDuration.value = d!.inSeconds.toDouble();
-//     });
-//     audioPlayer.positionStream.listen((p) {
-//       position(p.toString().split('.').first);
-//       value.value = p!.inSeconds.toDouble();
-//     });
-//   }
-//
-//   changeDurationToSeconds(seconds) {
-//     var duration = Duration(seconds: seconds);
-//     audioPlayer.seek(duration);
-//   }
-//
-//   playMusic(songs, index) {
-//     playIndex.value = index;
-//     Uri uriSong = Uri.parse(songs.uri.toString());
-//     print('Test : ' + songs.id.toString());
-//     try {
-//       audioPlayer.setAudioSource(AudioSource.uri(uriSong,
-//           tag: MediaItem(
-//               id: songs.id.toString(),
-//               title: songs.title,
-//               album: songs.album,
-//               artist: songs.artist)));
-//       audioPlayer.play();
-//       isPlaying(true);
-//       updatePosition();
-//     } catch (e) {
-//       print("Error");
-//     }
-//   }
-//
-//   checkPermission() async {
-//     var permission = await Permission.storage.request();
-//     if (permission.isGranted) {
-//     } else {
-//       print("Permission Denied");
-//       checkPermission();
-//     }
-//   }
-// }
+  return "$formattedHours$formattedMinutes:$formattedSeconds";
+}
